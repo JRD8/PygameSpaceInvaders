41dfad7eeefcbc3ccaa4887325b314fb1ff4f2e6
JRD8 <jdforbew@gmail.com>
2020-09-26 08:17:46 -0400

    Installed pyinstaller and worked through 1:20:38

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("JRD Space Invaders")
 pygame.font.init()
 
-
 # Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
@@ -25,6 +24,7 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background_black.png")), (WIDTH, HEIGHT))
+
 
 class Ship:
 	def __init__(self, x, y, health=100):
@@ -45,6 +45,26 @@ class Ship:
 
 	def get_height(self):
 		return self.ship_img.get_height()
+
+
+class Laser:
+	def __init__(self, x, y, img):
+		self.x = x
+		self.y = y
+		self.img = img
+		self.mask = pygame.mask.from_surface(self.img)
+
+	def draw(self, window):
+		window.blit(self.img, (self.x, self.y))
+
+	def move(self, vel):
+		self.y += vel
+
+	def off_screen(self.height):
+		return self.y <= height and self.y >= 0
+
+	def collision(self, obj):
+		return collide(obj, self)
 
 
 class Player(Ship):
@@ -70,6 +90,10 @@ class Enemy(Ship):
 
 	def move(self, vel):
 		self.y += vel
+
+
+def collide(obj1, obj2):
+	## START HERE
 
 
 def main():
